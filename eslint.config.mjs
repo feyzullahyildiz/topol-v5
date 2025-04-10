db4bf6f0f1fd6ed3a145520d120cf3fa7b49d947
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2025-04-10 11:39:42 +0300

    husky prettier eslint init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  prettierConfig,
+  prettierPlugin,
+  {
+    ignores: ['.next/**', 'node_modules/**', 'public/**', 'build/**', 'coverage/**'],
+  },
 ];
 
 export default eslintConfig;
